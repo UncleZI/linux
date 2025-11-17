f483cd486ff00b67e85a76b6a1a89e67c3ebcf44
Ivan Zhaldak <244457405+UncleZI@users.noreply.github.com>
2025-11-17 13:29:16 +0300

    Update quirks.c
    Add DEVICE_FLAG for IAG devices (0x2622) to enable DSD_RAW quirk

--- sound/usb/quirks.c
+++ sound/usb/quirks.c
@@ -2421,6 +2421,8 @@ static const struct usb_audio_quirk_flags_table quirk_flags_table[] = {
 		   QUIRK_FLAG_DSD_RAW),
 	VENDOR_FLG(0x25ce, /* Mytek devices */
 		   QUIRK_FLAG_DSD_RAW),
+	VENDOR_FLG(0x2622, /* IAG Limited devices */
+		   QUIRK_FLAG_DSD_RAW),
 	VENDOR_FLG(0x278b, /* Rotel? */
 		   QUIRK_FLAG_DSD_RAW),
 	VENDOR_FLG(0x292b, /* Gustard/Ess based devices */
